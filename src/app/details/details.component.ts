8a04231d90c1f597825ea1e37e43e89ca5ec5549
Daniel Fang <138417504+qkfang@users.noreply.github.com>
2025-03-02 09:12:14 +0000

    fix

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [],
   template: ``,
   styleUrls: ['./details.component.css'],
 })
 
 export class DetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = -1;
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 
