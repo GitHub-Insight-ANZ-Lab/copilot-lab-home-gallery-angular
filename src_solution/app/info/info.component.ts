a37c7ac52c5c8e55aafb15121f1eb06f7e555aaf
Daniel Fang <daniel.fang@insight.com>
2025-03-03 20:33:00 +1100

    add solution

---
+++ src_solution/app/info/info.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  imports: [],
+  template: `
+    <p>
+      info works!
+    </p>
+  `,
+  styles: ``
+})
+export class InfoComponent {
+
+}
